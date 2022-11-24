8f9841735dec49613efac763b70c19d7794445d4
Perry <fatboy2392328@gmail.com>
2022-11-23 19:17:36 -0500

    source files setup

---
+++ scr/assets/scripts/app.js
@@ -0,0 +1,20 @@
+/** @format */
+// Imports
+
+// import '../styles/main.css'
+import '../styles/main.scss'
+import React, { useState, useEffect, useRef } from 'react';
+// import ReactDOM from 'react-dom';//React 17
+import { createRoot } from 'react-dom/client'; //React 18
+
+// Javascript modules imports
+
+
+
+// begin Javascript code here
+
+
+//  Webpack browser refresh
+if (module.hot) {
+	module.hot.accept();
+}
